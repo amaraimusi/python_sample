963ef24ca71ce9d3efcdf44c19781c29bcda0d48
user <amaraimusi@gmail.com>
2021-08-08 10:40:23 +0900

    Pythonの連想配列 ディクショナリ | ディクショナリのforループ

--- a001/helloworld.py
+++ a001/helloworld.py
@@ -1,1 +1,15 @@
-print ('HelloWorld')
+print ('HelloWorld2')
+
+list = {'animal_name': '猫ちゃん', 'age': 3, 'place': '家の中'}
+
+print(list['animal_name'])
+
+for k, v in list.items():
+    print(k, v)
+    
+
+for v in list.values():
+    print(v)
+    
+for k in list.keys():
+    print(k)

77b91bd610160c624774ca42f6a3146027a7c14c
user <amaraimusi@gmail.com>
2021-08-11 22:41:43 +0900

    ファイル一覧取得の検証中

--- a001/readfile.py
+++ a001/readfile.py
@@ -4,16 +4,25 @@ import os
 cur_dp = os.getcwd()
 print (cur_dp)
 
-list = {'animal_name': '赤犬', 'age': 2, 'place': '家の外'}
+# fns = os.walk('C:\\')
+# print(fns)
 
-print(list['animal_name'])
+for curDir, dirs, files in os.walk('C:\\'):
+    print('===================')
+    print("現在のディレクトリ: " + curDir)
+    print("内包するディレクトリ:" + dirs)
+    print("内包するファイル: " + files)
 
-for k, v in list.items():
-    print(k, v)
-    
-
-for v in list.values():
-    print(v)
-    
-for k in list.keys():
-    print(k)
+# list = {'animal_name': '赤犬', 'age': 2, 'place': '家の外'}
+# 
+# print(list['animal_name'])
+# 
+# for k, v in list.items():
+#     print(k, v)
+#     
+# 
+# for v in list.values():
+#     print(v)
+#     
+# for k in list.keys():
+#     print(k)

4a3c7147d16dba48ee9ce7c49d411f53e8bf5f35
user <amaraimusi@gmail.com>
2021-09-12 10:27:46 +0900

    配列のソート

---
+++ a001/sort.py
@@ -0,0 +1,27 @@
+print ('ソート')
+
+list = [2,5,4,1,]
+list.sort()
+print(list);
+
+list2 = ['猫','abc','buta','akamusi','8']
+list2.sort()
+print(list2);
+
+# 数値と文字列が混じっているとエラーになる
+# list3 = [2,5,4,1,'abc']
+# list3.sort()
+# print(list3);
+
+print('逆順')
+
+list.reverse();
+print(list);
+
+list2.reverse();
+print(list2);
+
+
+
+
+

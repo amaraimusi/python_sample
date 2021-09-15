a6d4bd3138a1045fcae041b8ef74fa3928bf18d7
user <user@host.docker.internal>
2021-09-15 14:18:51 +0900

    pydubでmp3のフェードインとフェードアウト

--- ffmpeg_sample/test02_pydub_fadeout.py
+++ ffmpeg_sample/test02_pydub_fadeout.py
@@ -1,15 +1,14 @@
 print ('pydubでmp3のフェードインとフェードアウト')
 
-# -*- coding: utf-8 -*-
+# pydubを使うにはffmpegが必要-
+
 from pydub import AudioSegment
-# -*- coding: utf-8 -*-
-#import pydub
 
-# 音声ファイルの読み込み
-sound = AudioSegment.from_file("./test_data/test1.mp3", "mp3")
 
-# フェードイン（3秒）、フェードアウト（4秒）
-sound1 = sound.fade_in(4000).fade_out(4000)
+audio = AudioSegment.from_file("./test_data/test1.mp3", "mp3")
 
-# 保存
-sound1.export("./test_data/output02.mp3", format="mp3")
+# フェードイン4秒、フェードアウト3.5秒に変換したオーディオオブジェクトを作成
+audio2 = audio.fade_in(4000).fade_out(3500)
+
+# 変換したオーディオオブジェクトを出力
+audio2.export("./test_data/output02.mp3",  format="mp3")

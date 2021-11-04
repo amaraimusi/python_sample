78c0dbca814100c1a4bdb8283bf52ecfbdb109eb
user <user@host.docker.internal>
2021-11-05 08:08:53 +0900

    Pythonの覚書：文字存在チェック | 指定文字が含まれるか

---
+++ a001/in_string.py
@@ -0,0 +1,11 @@
+print ('Pythonの覚書：文字存在チェック | 指定文字が含まれるか')
+
+res = 'p' in 'python' 
+print(res)
+
+res = '大猫' in '臆病な大猫に目を合わせてはいけません' 
+print(res)
+
+res = '犬' in '臆病な大猫に目を合わせてはいけません' 
+print(res)
+

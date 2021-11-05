824708218a9ebaeb604ff8aa2ca5933822d476ae
user <amaraimusi@gmail.com>
2021-11-05 12:54:29 +0900

    tool01_mp4_to_mp3

---
+++ tool/tool01_mp4_to_mp3/tool01_mp4_to_mp3.py
@@ -0,0 +1,13 @@
+print ('XXX')
+
+# pydubを使うにはffmpegが必要-
+from ..ffmpeg_sample.pydub import AudioSegment
+
+# # mp4ファイルの読み込み
+# audio = AudioSegment.from_file("./test_data/MVI_0887.MP4", "mp4")
+# 
+# # mp3としてエクスポートする
+# audio.export("./test_data/output03.mp3", format="mp3")
+
+print('Success!')
+

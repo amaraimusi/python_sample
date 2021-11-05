b4f07fdf8014febf9d2e8ccd97b9490a6b34177c
user <user@host.docker.internal>
2021-11-05 09:54:56 +0900

    文字列を検索して取得

---
+++ a001/get_conf_from_textfile.py
@@ -0,0 +1,6 @@
+print ('iniファイルなどから設定を読み込む | conf')
+
+getConfFromTextfile()
+
+def getConfFromTextfile(text_fn):
+    print text_fn
---
+++ a001/string_find.py
@@ -0,0 +1,80 @@
+print ('Pythonの覚書：文字列を検索して取得')
+
+
+
+# 文字列を左側から印文字を検索し、左側の文字を切り出す
+# @param string s 対象文字列
+# @param $mark 印文字
+# @return 印文字から左側の文字列
+def stringLeft(s, mark):
+    a =s.find(mark)
+    res = s[0:a]
+    return res
+
+
+# 文字列を左側から印文字を検索し、右側の文字を切り出す。
+# @param string s 対象文字列
+# @param $mark 印文字
+# @return 印文字から右側の文字列
+def stringRight(s, mark):
+    a =s.find(mark)
+    res = s[a+len(mark):]
+    return res
+
+# 文字列を右側から印文字を検索し、左側の文字を切り出す
+# @param string s 対象文字列
+# @param $mark 印文字
+# @return 印文字から左側の文字列
+def stringLeftRev(s, mark):
+    a =s.rfind(mark)
+    res = s[a+len(mark):]
+    return res
+
+
+# 文字列を右側から印文字を検索し、左側の文字を切り出す
+# @param string s 対象文字列
+# @param $mark 印文字
+# @return 印文字から左側の文字列
+def stringRightRev(s, mark):
+    a =s.rfind(mark)
+    res = s[0:a]
+    return res
+
+
+
+
+
+print('-----------')
+
+print('#1 stringLeft:文字列を左側から印文字を検索し、左側の文字を切り出す')
+str= 'ブタ:ネコ:ライオン:シカ'
+res = stringLeft(str, ':')
+print(str)
+print(res)
+
+
+print('-----------')
+
+print('#2 stringRight:文字列を左側から印文字を検索し、右側の文字を切り出す。')
+str= 'ブタ:ネコ:ライオン:シカ'
+res = stringRight(str, ':')
+print(str)
+print(res)
+
+print('-----------')
+
+print('#3 stringLeftRev:文字列を右側から印文字を検索し、左側の文字を切り出す')
+str= 'ブタ:ネコ:ライオン:シカ'
+res = stringLeftRev(str, ':')
+print(str)
+print(res)
+
+print('-----------')
+
+print('#4 stringRightRev:文字列を右側から印文字を検索し、左側の文字を切り出す')
+str= 'ブタ:ネコ:ライオン:シカ'
+res = stringRightRev(str, ':')
+print(str)
+print(res)
+
+print('-----------')

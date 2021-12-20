1f884062d354bd92857aa67d2476e858bc0aa6ef
user <user@DESKTOP-PE1E9IO.mshome.net>
2021-12-20 14:59:57 +0900

    tkinterの基本←修正

--- a001/tkinter/helloworld.py
+++ a001/tkinter/helloworld.py
@@ -13,15 +13,17 @@ def btn_click():
     
 root.geometry('640x480') # メインウィンドウのサイズ
 
-btn2 = tk.Button(root, text = '犬ボタン', command=btn_click).grid(row= 0, column=2)
-btn3 = tk.Button(root, text = '牛ボタン').grid(row= 0, column=3)
+btn = tk.Button(root, text = '犬ボタン', command=btn_click)
+btn.grid(row= 0, column=1)
+lbl = tk.Label(root, text = '動物名')
+lbl.grid(row= 0, column=2)
 
 textbox = tk.Text(width= 90, height= 1)
-textbox.grid(row= 0, column=4)
+textbox.grid(row= 0, column=3)
 textbox.insert('1.0', '赤い猫のエサ場')
 
-btn4 = tk.Button(root, text = '山羊ボタン').grid(row= 1, column=2)
-btn5 = tk.Button(root, text = '獅子ボタン').grid(row= 1, column=3)
-btn6 = tk.Button(root, text = 'ヤンバルテナガコガネボタン').grid(row= 1, column=4)
+btn4 = tk.Button(root, text = '山羊ボタン').grid(row= 1, column=1)
+btn5 = tk.Button(root, text = '獅子ボタン').grid(row= 1, column=2)
+btn6 = tk.Button(root, text = 'ヤンバルテナガコガネボタン').grid(row= 1, column=3)
 
 root.mainloop() # ウィンドウを表示
